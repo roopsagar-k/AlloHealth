957614ce35b7ee93962aa683806d0e8b336d0680
roopsagar-k <roopsagarudayar9854@gmail.com>
2025-01-25 14:30:24 +0530

    base url update

--- clinic-frontend/src/lib/axiosInstance.ts
+++ clinic-frontend/src/lib/axiosInstance.ts
@@ -2,6 +2,6 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:3001"
+  baseURL: process.env.BACKEND_BASEURL ||"http://allo-api.roopsagar.tech"
 });
 export default axiosInstance;
